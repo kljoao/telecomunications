210e789222bea89b0281fc7b55f7a8b7afec0343
João Ramos <joao.ramos@infocrerj.local>
2023-10-26 17:03:29 -0300

    first commit

---
+++ index.php
@@ -0,0 +1,56 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+
+<head>
+    <meta charset="UTF-6">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Oficial Telecall</title>
+    <!-- Fonts API -->
+    <script src="https://kit.fontawesome.com/4713c304f5.js" crossorigin="anonymous"></script>
+    <!-- Fonts API -->
+    <!-- Google Icons -->
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@20..46,100..700,0..1,-50..200" />
+    <!-- Google Icons -->
+    <link rel="stylesheet" href="style.css">
+</head>
+
+<body>
+
+    <nav class="nav">
+        <div class="nav-container">
+            <img src="assets/img/logo-head.png" alt="" width="175px">
+            <ul class="nav-options">
+                <a href="" class="medium-16-blue effect-1"><li>Internet</li></a>
+                <a href="" class="medium-16-blue effect-1"><li>Telefonia</li></a>
+                <a href="" class="medium-16-blue effect-1"><li>Móvel</li></a>
+                <a href="" class="medium-16-blue effect-1"><li>Redes</li></a>
+            </ul>
+        </div>
+        <div class="nav-left">
+            <a href=""><button class="nav-button"><i class="fa-solid fa-user"></i><p>Area do Cliente</p></button></a>
+            <div class="nav-contact">
+            <span class="material-symbols-outlined" style="color: grey; font-size: 35px;">headset_mic</span>
+                <div>
+                    <p class="small-12-grey">Fale com o nosso time</p>
+                    <p class="medium-16-blue">(21) xxxxx-xxxx</p>
+                </div>
+            </div>
+        </div>
+    </nav>
+
+    <main>
+        <div class="index-main-content">
+            <h1 class="hight-72-white">T E L E C A L L</h1>
+        </div>
+        <div class="index-main-letters">
+            <p class="medium-12-white index-main-p">A solução completa você encontra aqui</p>
+        </div>
+        <div class="index-main-button">
+            <a href=""><button class="index-main-btn">SAIBA MAIS<span></span><span></span><span></span><span></span></button></a>
+        </div>
+        <img src="assets/img/family.png" width="100%" height="100%" max-width="1903px" max-height="950px">
+    </main>
+
+</body>
+
+</html>
